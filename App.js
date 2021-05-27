dea192cdcc50d34a5b0ef903beefcbb88b03dd54
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-27 20:02:40 +0530

    add style component like css

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from "react";
+import {View,TextInput,Button,Text,StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component
+{
+  render()
+  {
+    return(
+      <View style={styles.container}>
+        <View style={styles.header}>
+
+          <Text style={styles.boldText}>Hello world</Text>
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.boldText}>Ishan Reshmika <Text>1997</Text> Ediriweera </Text>
+          <Text>Ishan Reshmika</Text>
+          <Text>Ishan Reshmika</Text>
+          <Text>Ishan Reshmika</Text>
+
+        </View>
+        
+      
+       
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
   },
+  header:{
+    backgroundColor:'pink',
+    padding:20,
+  },
+  boldText:{
+    fontWeight:'bold',
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+     
+  }
+
+
 });
