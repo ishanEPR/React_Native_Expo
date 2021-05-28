e1511d094cb272fe35e415a29094a5de98d429a9
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-28 21:12:46 +0530

    add new todo and display it

--- App.js
+++ App.js
@@ -22,14 +22,21 @@ export default function App()
     });
   }
 
-
+const submitHandler = (text) =>{
+  setTodos((prevTodos)=>{
+    return [
+      {text:text,key:Math.random().toString()},
+      ...prevTodos
+    ];
+  })
+}
 
  
     return(
       <View style={styles.container}>
      <Header/>
         <View style={styles.content}>
-          <AddTodo/>
+          <AddTodo submitHandler={submitHandler}/>
           <View style={styles.list}>
             <FlatList
             data={todos}
--- components/AddTodo.js
+++ components/AddTodo.js
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import {Text,StyleSheet,TextInput,Button,View} from 'react-native';
 
 
-export default function AddTodo()
+export default function AddTodo({submitHandler})
 {
     const  [text,setText]=useState('');
     const changeHandler = (val) =>{
@@ -16,7 +16,7 @@ export default function AddTodo()
             placeholder='new todo...'
             onChangeText={changeHandler}
             />
-            <Button onPress={()=>console.log(text)} title='add todo' color='coral'/>
+            <Button onPress={()=>submitHandler(text)} title='add todo' color='coral'/>
         </View>
     );
 }
