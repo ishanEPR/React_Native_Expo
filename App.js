9a58095d39e4cf1a7452d5931be27372da0799bb
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-15 22:21:45 +0530

    Tutorial-3

--- App.js
+++ App.js
@@ -1,95 +1,136 @@
-import React,{useState} from "react";
-import {View,Text,StyleSheet,FlatList,Alert,TouchableWithoutFeedback,keyboard} from 'react-native';
-import Header from './components/Header';
-import TodoItem from './components/TodoItem';
-import AddTodo from './components/AddTodo';
+// import React,{useState} from "react";
+// import {View,Text,StyleSheet,FlatList,Alert,TouchableWithoutFeedback,keyboard} from 'react-native';
+// import Header from './components/Header';
+// import TodoItem from './components/TodoItem';
+// import AddTodo from './components/AddTodo';
 
-export default function App()
-{
+// export default function App()
+// {
 
-  const [todos,setTodos]=useState(
-    [
-      {text:'buy coffe',key:'1'},
-      {text:'create an app',key:'2'},
-      {text:'play on the switch',key:'3'},
-      {text:'my todos',key:'4'  }
-    ]
-  );
+//   const [todos,setTodos]=useState(
+//     [
+//       {text:'buy coffe',key:'1'},
+//       {text:'create an app',key:'2'},
+//       {text:'play on the switch',key:'3'},
+//       {text:'my todos',key:'4'  }
+//     ]
+//   );
 
-  const pressHandler=(key) =>{
+//   const pressHandler=(key) =>{
 
-    setTodos((prevTodos)=>{
-      return prevTodos.filter(todo => todo.key != key);
-    });
-  }
+//     setTodos((prevTodos)=>{
+//       return prevTodos.filter(todo => todo.key != key);
+//     });
+//   }
 
-const submitHandler = (text) =>{
+// const submitHandler = (text) =>{
 
-  if (text.length>3) {
-    setTodos((prevTodos)=>{
-    return [
-      {text:text,key:Math.random().toString()},
-      ...prevTodos
-    ];
-  })
-  }else{
-    Alert.alert('OOPS!','Todos must be over 3 chars long',[
-      {
-        text:'Understood',onPress:()=>console.log('alert closed')
-      }
-    ]);
+//   if (text.length>3) {
+//     setTodos((prevTodos)=>{
+//     return [
+//       {text:text,key:Math.random().toString()},
+//       ...prevTodos
+//     ];
+//   })
+//   }else{
+//     Alert.alert('OOPS!','Todos must be over 3 chars long',[
+//       {
+//         text:'Understood',onPress:()=>console.log('alert closed')
+//       }
+//     ]);
 
-  }
+//   }
   
-}  
+// }  
 
  
-    return(
-      <TouchableWithoutFeedback
-      onPress={()=>{
-        keyboard.dismiss();
-        console.log("dismissed keyboard")
+//     return(
+//       <TouchableWithoutFeedback
+//       onPress={()=>{
+//       //  keyboard.dismiss();
+//         console.log("dismissed keyboard")
 
-      }}>
-          <View style={styles.container}>
-            <Header/>
-            <View style={styles.content}>
-              <AddTodo submitHandler={submitHandler}/>
-              <View style={styles.list}>
-                <FlatList
-                data={todos}
-                renderItem={({item})=>(
-                <TodoItem item={item} pressHandler={pressHandler}/>
-                )}
+//       }}>
+//           <View style={styles.container}>
+//             <Header/>
+//             <View style={styles.content}>
+//               <AddTodo submitHandler={submitHandler}/>
+//               <View style={styles.list}>
+//                 <FlatList
+//                 data={todos}
+//                 renderItem={({item})=>(
+//                 <TodoItem item={item} pressHandler={pressHandler}/>
+//                 )}
                 
-                />
+//                 />
 
-              </View>
-            </View>
+//               </View>
+//             </View>
     
 
 
-          </View>
-      </TouchableWithoutFeedback>
-    );
+//           </View>
+//       </TouchableWithoutFeedback>
+//     );
   
+// }
+// const styles=StyleSheet.create({
+//   container:{
+//     flex:1,
+//     backgroundColor:'#fff',
+//     // paddingTop:40,
+//     // paddingHorizontal:20,
+//     // alignItems:'center',
+//     // justifyContent:'center',
+//   },
+//   content:{
+//     padding:40,
+//   },
+//   list:{
+//     marginTop:20,
+//   }
+ 
+
+
+// });
+
+
+
+////////////learn
+import React from 'react';
+import {StyleSheet,Text,Button,View} from 'react-native';
+
+export default function App()
+{
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello Ishan</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Hello Ishan</Text>
+      </View>
+    </View>
+  );
 }
+
 const styles=StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'#fff',
-    // paddingTop:40,
-    // paddingHorizontal:20,
-    // alignItems:'center',
-    // justifyContent:'center',
+    alignItems:'center',
+    justifyContent:'center',
   },
-  content:{
-    padding:40,
+  header:{
+    backgroundColor:"pink",
+    padding:20,
   },
-  list:{
-    marginTop:20,
+  boldText:{
+    fontWeight:'bold',
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+    
   }
- 
-
-
-});
+})
