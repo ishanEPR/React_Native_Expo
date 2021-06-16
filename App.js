d669da23f8fa2a68191b08090d07e6f6994486ce
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-06-16 16:10:02 +0530

    Tutorial-9-Todo-app

--- App.js
+++ App.js
@@ -99,78 +99,39 @@
 ////////////learn
 import React,{useState} from 'react';
 import {StyleSheet,Text,Button,View,FlatList,TouchableOpacity} from 'react-native';
+import Header from './components/Header';
 
 
 
 
 export default function App()
 {
-  const [people,setPeople]=useState([
-    {name:'Ishan',age:'23',key:'1'},
-    {name:'Ishan1',age:'24',key:'2'},
-    {name:'Ishan2',age:'25',key:'3'},
-    {name:'Ishan3',age:'26',key:'4'},
-    {name:'Ishan4',age:'27',key:'5'},
-    {name:'Ishan6',age:'27',key:'6'},
-    {name:'Ishan7',age:'27',key:'7'},
-    {name:'Ishan8',age:'27',key:'8'},
-    
-  ]);
+  const [todos,setTodos]=useState([
+    {text:'buy coffee',key:'1'},
+    {text:'create an app',key:'2'},
+    {text:'play on the switch',key:'3'},
+  ])
   
 
-  const pressHandler=(id)=>{
-    console.log(id);
 
-    setPeople((prevPeople)=>{
-
-      return prevPeople.filter(person =>person.key!=id)
-    })
-  }
- 
   return (
     <View style={styles.container}>
+    <Header/>
+      <View style={styles.content}>
 
-    <FlatList
-
-    numColumns={2}
-    keyExtractor={(item)=>item.key}
-    
-    data={people}
-    renderItem={({item})=>(
-
-      <TouchableOpacity
+      {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
       
-      onPress={()=>pressHandler(item.key)}
-      >
-        <Text style={styles.item}>{item.name}={item.age}</Text>
-
-      </TouchableOpacity>
-
-      
-    )}
-    />
-
-    {/* <ScrollView>
-
-         {
-      people.map((data)=>(
-          <View key={data.key}>
-            <Text style={styles.item}>{data.name} and {data.age}</Text>
-          </View>
-      )
-        
-
-          
-        
-      )
-    }
-    
-    </ScrollView> */}
+      </View>
 
 
-   
-   
-   
     </View>
   );
 }
@@ -179,17 +140,16 @@ const styles=StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'#fff',
-   paddingTop:40,
-   paddingHorizontal:20
+  
   },
-  item:{
+  content:{
    
-    padding:30,
-    marginTop:24,
-    backgroundColor:'pink',
-    fontSize:24,
-    marginHorizontal:10
+    padding:40,
    
+   
+  },
+  list:{
+    marginTop:20
   }
   
  })
